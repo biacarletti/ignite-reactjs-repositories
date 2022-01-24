97c59eb194dca0938302b9e27528d20550323ea3
Beatriz da Silva Carletti <Bia_carletti@hotmail.com>
2022-01-24 10:25:42 -0300

    realizando o fast-refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-module.exports ={
+module.exports = {
     mode: isDevelopment ? 'development' : 'production',
-    devtool:  isDevelopment ?'eval-source-map' : 'source-map',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -15,21 +16,32 @@ module.exports ={
         extensions: ['.js', '.jsx'],
     },
     devServer: {
-        static:{
+        static: {
             directory: path.join(__dirname, 'public'),
+            staticOptions: {
+                hot: true
+            }
         }
     },
     plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
+    ].filter(Boolean),
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_module/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                },
             },
             {
                 test: /\.scss$/,
